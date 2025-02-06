3dbe451ea0eb3d16657c49f163d8c13394c31eba
Alessio Guacci <alessioguacci@gmail.com>
2025-02-06 20:02:42 +0100

    updated VeicoliService

--- src/main/java/com/example/carCatalogService/Service/VeicoliService.java
+++ src/main/java/com/example/carCatalogService/Service/VeicoliService.java
@@ -1,11 +1,51 @@
 package com.example.carCatalogService.Service;
 
+import com.example.carCatalogService.DTO.VeicoloDTO;
+import com.example.carCatalogService.DTO.VeicoloRequestDTO;
+import com.example.carCatalogService.Model.Veicolo;
 import com.example.carCatalogService.Repository.VeicoliRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VeicoliService {
-    @Autowired
-    private VeicoliRepository veicoliRepository;
+
+    private final VeicoliRepository veicoliRepository;
+
+    public VeicoliService(VeicoliRepository veicoliRepository) {
+        this.veicoliRepository = veicoliRepository;
+    }
+
+    public List<VeicoloDTO> prentiTuttiVeicoli(){
+        List<Veicolo> veicoli = veicoliRepository.findAll();
+        List<VeicoloDTO>veicoloDTOS = new ArrayList<>();
+        for (Veicolo veicolo: veicoli){
+            veicoloDTOS.add(VeicoloDTO.daEntita(veicolo));
+        }
+        return veicoloDTOS;
+    }
+
+    public VeicoloDTO getVeicoloDaID (long id){
+        Veicolo veicolo= veicoliRepository.findById(id).orElseThrow(()
+                ->new RuntimeException("Veicolo non trovato"));
+        return VeicoloDTO.daEntita(veicolo);
+    }
+
+    public VeicoloDTO aggiungiVeicolo (VeicoloRequestDTO veicoloRequestDTO){
+        Veicolo veicolo = veicoloRequestDTO.transformazioneInEntita();
+        return VeicoloDTO.daEntita(veicoliRepository.save(veicolo));
+    }
+
+    public void cancellaVeicoloDaID (long id){
+        Veicolo veicolo= veicoliRepository.findById(id).orElseThrow(()
+                -> new RuntimeException("Veicolo non trovato"));
+        veicoliRepository.delete(veicolo);
+    }
+
+    public void cancellaTuttiVeicoli(){
+        veicoliRepository.deleteAll();
+    }
+
 }

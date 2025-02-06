6db5b60e665c4d0a75a2da5ecc77358265bc15c2
Alessio Guacci <alessioguacci@gmail.com>
2025-02-06 19:09:27 +0100

    updated VeicoliRepository.java

--- src/main/java/com/example/carCatalogService/Repository/VeicoliRepository.java
+++ src/main/java/com/example/carCatalogService/Repository/VeicoliRepository.java
@@ -1,9 +1,13 @@
 package com.example.carCatalogService.Repository;
 
+import com.example.carCatalogService.Model.Enum.StatoMacchina;
 import com.example.carCatalogService.Model.Veicolo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface VeicoliRepository extends JpaRepository<Veicolo,Long> {
+    List<Veicolo> ricercaPerMarca(String marca);
+    List<Veicolo> ricercaPerRangeDiPrezzo(double prezzoMin, double prezzoMax);
+    List<Veicolo> ricercaPerStato(StatoMacchina stato);
 }

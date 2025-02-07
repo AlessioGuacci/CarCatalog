c7dbd6f7228cb841133dcedb8238f0eb23f0d571
Alessio Guacci <alessioguacci@gmail.com>
2025-02-07 12:47:10 +0100

    Finished Controller Branch and Tested endpoints through postman

--- src/main/java/com/example/carCatalogService/Controller/VeicoliController.java
+++ src/main/java/com/example/carCatalogService/Controller/VeicoliController.java
@@ -1,9 +1,53 @@
 package com.example.carCatalogService.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.carCatalogService.DTO.ResponeMessageDTO;
+import com.example.carCatalogService.DTO.VeicoloDTO;
+import com.example.carCatalogService.DTO.VeicoloRequestDTO;
+import com.example.carCatalogService.Model.Veicolo;
+import com.example.carCatalogService.Service.VeicoliService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/veicoli")
 public class VeicoliController {
+
+    private final VeicoliService veicoliService;
+
+    public VeicoliController(VeicoliService veicoliService) {
+        this.veicoliService = veicoliService;
+    }
+
+    @GetMapping("/tutti")
+    public List<VeicoloDTO>tuttiVeicolci(){
+       return veicoliService.prentiTuttiVeicoli();
+    }
+
+    @GetMapping("/seleziona{id}")
+    public VeicoloDTO sceglieVehicolo(@PathVariable long id){
+       return veicoliService.getVeicoloDaID(id);
+    }
+
+    @PostMapping("/aggiungi")
+    public VeicoloDTO aggiungiVeicolo(@Valid @RequestBody VeicoloRequestDTO veicoloRequestDTO){
+        return veicoliService.aggiungiVeicolo(veicoloRequestDTO);
+    }
+
+    @DeleteMapping("/elimina{id}")
+    public ResponseEntity<ResponeMessageDTO> eliminaVeicolo(@PathVariable long id){
+        veicoliService.cancellaVeicoloDaID(id);
+        return ResponeMessageDTO.success("Il veicolo è stato elimanto");
+    }
+
+    @DeleteMapping("/eliminaTutti")
+    public ResponseEntity<ResponeMessageDTO>eliminaTuttiVeicoli(){
+        veicoliService.cancellaTuttiVeicoli();
+        return ResponeMessageDTO.success("Tutti I veicoli sono stati eliminati");
+    }
+
+
 }
